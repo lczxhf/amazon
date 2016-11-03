1de33afc509a1a00fcce68cff8630ba8e4b1b905
林 梓浩 <lzh@rinteki-MacBook-Air.local>
2016-11-03 12:11:43 +0800

    ok

--- js/controllers/check_list.js
+++ js/controllers/check_list.js
@@ -6,6 +6,16 @@ app.controller('check_list', ['$scope','az','tips',function($scope,az,tips) {
   },function(data){
       $scope.data = data.data;
       $scope.token = getCookie('token')
+      for (var i = 0; i < $scope.data.length; i++) {
+        $scope.data[i].done_time = 0
+        var relations = $scope.data[i].check_with_takopet_relations
+        for (var j = 0; j < relations.length; j++) {
+           if(relations[j].task_operate_relation.status == 3){
+             $scope.data[i].done_time++
+           }
+        }
+
+      }
   });
   $scope.host = HOST
   $scope.getStatus = function(status){
