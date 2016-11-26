5a5ecbc7a44ea03558de4e890deb99d35e7729f4
林 梓浩 <lzh@rinteki-MacBook-Air.local>
2016-11-26 18:05:22 +0800

    ok

--- js/controllers/check_list.js
+++ js/controllers/check_list.js
@@ -1,22 +1,30 @@
 app.controller('check_list', ['$scope','az','tips',function($scope,az,tips) {
-  az.check_list({
-      token:getCookie('token'),
-      page:1,
-      per_page:500
-  },function(data){
-      $scope.data = data.data;
-      $scope.token = getCookie('token')
-      for (var i = 0; i < $scope.data.length; i++) {
-        $scope.data[i].done_time = 0
-        var relations = $scope.data[i].check_with_takopet_relations
-        for (var j = 0; j < relations.length; j++) {
-           if(relations[j].task_operate_relation.status == 3){
-             $scope.data[i].done_time++
-           }
-        }
+  $scope.data = []
 
-      }
-  });
+  $scope.get_list = function(page){
+    az.check_list({
+        token:getCookie('token'),
+        page:page,
+        per_page:10
+    },function(data){
+        $scope.data = $scope.data.concat(data.data);
+        $scope.token = getCookie('token')
+        page++
+        if(page < 20){
+          $scope.get_list(page)
+        }
+        for (var i = 0; i < $scope.data.length; i++) {
+          $scope.data[i].done_time = 0
+          var relations = $scope.data[i].check_with_takopet_relations
+          for (var j = 0; j < relations.length; j++) {
+             if(relations[j].task_operate_relation.status == 3){
+               $scope.data[i].done_time++
+             }
+          }
+        }
+    });
+  }
+  $scope.get_list(1)
   $scope.host = HOST
   $scope.getStatus = function(status){
     switch (status) {
--- js/controllers/task_excuted.js
+++ js/controllers/task_excuted.js
@@ -1,21 +1,30 @@
 app.controller('task_excuted', ['$scope','az','tips',function($scope,az,tips) {
-  az.task_excuted({
-      token:getCookie('token'),
-      page:1,
-      per_page:20
-  },function(data){
-      $scope.data = data.data;
-      // for (var i = 0; i < $scope.data.length; i++) {
-      //   $scope.data[i].done_time = 0
-      //   var relations = $scope.data[i].check_with_takopet_relations
-      //   for (var j = 0; j < relations.length; j++) {
-      //      if(relations[j].task_operate_relation.status == 3){
-      //        $scope.data[i].done_time++
-      //      }
-      //   }
-      //
-      // }
-  });
+  $scope.data = []
+
+  $scope.get_list = function(page){
+    az.task_excuted({
+        token:getCookie('token'),
+        page:page,
+        per_page:10
+    },function(data){
+        $scope.data = $scope.data.concat(data.data);
+        page++
+        if(page<20){
+          $scope.get_list(page)
+        }
+        // for (var i = 0; i < $scope.data.length; i++) {
+        //   $scope.data[i].done_time = 0
+        //   var relations = $scope.data[i].check_with_takopet_relations
+        //   for (var j = 0; j < relations.length; j++) {
+        //      if(relations[j].task_operate_relation.status == 3){
+        //        $scope.data[i].done_time++
+        //      }
+        //   }
+        //
+        // }
+    });
+  }
+  $scope.get_list(1)
   $scope.getStatus = function(status){
     switch (status) {
       case 1:
